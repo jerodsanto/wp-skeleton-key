cc0f28979678859d602e51ec65ab64ad4fa21197
Douglas Neiner <doug@Douglas-Neiners-Laptop.local>
2009-10-30 03:20:36 +0800

    Optimized queries and reduced lookups for admin logins to one call total. Fixed links and updated change log. Bumped version to 1.0.1

--- wp-skeleton-key.php
+++ wp-skeleton-key.php
@@ -5,7 +5,7 @@
 * Description: Gives administrators a skeleton key (their own password) to login as any user they'd like.
 * Author: Jerod Santo
 * Author URI: http://jerodsanto.net
-* Version: 1.0
+* Version: 1.0.1
 * */
 
 // HOOK ME UP
@@ -20,11 +20,9 @@ function authenticate_with_skeleton_key($user, $username, $password) {
 
     foreach($admin_logins as $login) {
       // we don't want to handle admins logging in as themselves
-      if ($username == $login) { break; }
-      
-      $admindata = get_userdatabylogin($login);
+      if ($username == stripslashes($login->user_login)) { break; }
 
-      if (wp_check_password($password, $admindata->user_pass, $admindata->ID)) {
+      if (wp_check_password($password, $login->user_pass, $login->ID)) {
         return new WP_User($userdata->ID);
       }
     }
@@ -35,14 +33,14 @@ function authenticate_with_skeleton_key($user, $username, $password) {
 function get_logins_for_userlevel($level) {
   global $wpdb;
 
-  $logins = array();
-  $results = $wpdb->get_results("SELECT user_id from $wpdb->usermeta 
-    WHERE meta_key='wp_user_level' and meta_value='$level'");
+  $logins = $wpdb->get_results(
+	"SELECT $wpdb->users.ID, $wpdb->users.user_login, $wpdb->users.user_pass " .
+	"FROM $wpdb->users, $wpdb->usermeta " .
+	"WHERE $wpdb->usermeta.meta_key   = 'wp_user_level' " .
+	  "AND $wpdb->usermeta.meta_value = '$level' " .
+	  "AND $wpdb->usermeta.user_id    = $wpdb->users.ID;"
+	);
 
-  foreach($results as $r) {
-    $result = $wpdb->get_results("SELECT user_login from $wpdb->users WHERE ID='$r->user_id'");
-    array_push($logins, stripslashes($result[0]->user_login));
-  }
   return $logins;
 }
 
